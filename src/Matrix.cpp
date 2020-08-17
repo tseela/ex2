88397581d6938b57524df9531b464be886a1844d
Yair Koskas <yairi2003@gmail.com>
2020-08-17 12:50:49 +0300

    added implementation for the copy ctor, move ctor, assignement operator

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -6,28 +6,17 @@ class Matrix {
     public:
         Matrix(uint32_t height, uint32_t width) {
             matrix_create(&m_matrix, height, width);
-
-
-
         }
         Matrix(const Matrix& m) {
-            
-
-
-
+            m_matrix = m.m_matrix;
         }
         
-        Matrix& operator=(const Matrix& m) {
-
-
-
-
+        Matrix& operator=(const Matrix& other) {
+            m_matrix = other.m_matrix;
+            return *this;
         }
         Matrix(Matrix&& m) noexcept {
-
-
-
-
+            m_matrix = m.m_matrix;
         }
 
         Matrix& operator=(Matrix&& m) noexcept {
@@ -38,68 +27,32 @@ class Matrix {
         }
         ~Matrix() {
             matrix_destroy(m_matrix);
-            
-
         }
 
-        Matrix copy(CPMatrix copy) {
-
-
-
-
-
-
-
-        }
         uint32_t getHeight() {
             uint32_t height;
             matrix_getHeight(m_matrix, &height);
             return height;
-
-
-
         }
         uint32_t getWidth() {
             uint32_t width;
             matrix_getWidth(m_matrix, &width);
             return width;
-
-
-
-
         }
         void setValue(uint32_t rowIndex, uint32_t colIndex, double value) {
             matrix_setValue(m_matrix,rowIndex,colIndex,value);
-
-
-
-
-
-
         }
         const Matrix add(CPMatrix lhs, CPMatrix rhs) {
             PMatrix m;
             matrix_add(&m,lhs,rhs);
             return *m;
-
-
-
         }
         const Matrix multMatrix(CPMatrix lhs, CPMatrix rhs) {
             PMatrix m;
             matrix_multiplyMatrices(&m,lhs,rhs);
             return *m;
-
-
-
         }
         const void multMatrixWithScalar(PMatrix m,double scalar) {
             matrix_multiplyWithScalar(m,scalar);
-            
-
-
-
-
-
         }
 };
