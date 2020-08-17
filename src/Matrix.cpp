6e3383d99e78b7f049961bfdb57034fb331cb71b
Yair Koskas <yairi2003@gmail.com>
2020-08-17 13:48:38 +0300

    added correct if conditions

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -22,16 +22,16 @@ class Matrix {
 
     public:
         Matrix(uint32_t height, uint32_t width) throw (MatrixException) {
-            if (matrix_create(&m_matrix, height, width))
+            if (!error_isSuccess(matrix_create(&m_matrix, height, width)))
                 throw new MatrixException(matrix_create(&m_matrix, height, width));
         }
         Matrix(const Matrix& m) throw (MatrixException) {
-            if (matrix_copy(&m_matrix, m.m_matrix))
+            if (!error_isSuccess(matrix_copy(&m_matrix, m.m_matrix)))
                 throw new MatrixException(matrix_copy(&m_matrix, m.m_matrix));
         }
         
         Matrix& operator=(const Matrix& other) throw (MatrixException) {
-            if (matrix_copy(&m_matrix, other.m_matrix))
+            if (!error_isSuccess(matrix_copy(&m_matrix, other.m_matrix)))
                 throw new MatrixException(matrix_copy(&m_matrix, other.m_matrix));
             return *this;
         }
@@ -51,36 +51,36 @@ class Matrix {
 
         uint32_t getHeight() throw (MatrixException) {
             uint32_t height;
-            if (matrix_getHeight(m_matrix, &height))
+            if (!error_isSuccess(matrix_getHeight(m_matrix, &height)))
                 throw new MatrixException(matrix_getHeight(m_matrix, &height));
             return height;
         }
         uint32_t getWidth() throw (MatrixException) {
             uint32_t width;
-            if (matrix_getWidth(m_matrix, &width))
+            if (!error_isSuccess(matrix_getWidth(m_matrix, &width)))
                 throw new MatrixException(matrix_getWidth(m_matrix, &width));
             return width;
         }
         void setValue(uint32_t rowIndex, uint32_t colIndex, double value) throw (MatrixException) {
-            if (matrix_setValue(m_matrix, rowIndex, colIndex, value))
+            if (!error_isSuccess(matrix_setValue(m_matrix, rowIndex, colIndex, value)))
                 throw new MatrixException(matrix_setValue(m_matrix, rowIndex, colIndex, value));
         }
         const Matrix add(CPMatrix lhs, CPMatrix rhs) throw (MatrixException) {
             PMatrix m;
             ErrorCode ec = matrix_add(&m, lhs, rhs);
-            if (ec)
+            if (!error_isSuccess(ec))
                 throw new MatrixException(ec);
             return *m;
         }
         const Matrix multMatrix(CPMatrix lhs, CPMatrix rhs) throw (MatrixException) {
             PMatrix m;
             ErrorCode ec = matrix_multiplyMatrices(&m, lhs, rhs);
-            if (ec)
+            if (!error_isSuccess(ec))
                 throw new MatrixException(ec);
             return *m;
         }
         void multMatrixWithScalar(double scalar) throw (MatrixException) {
-            if (matrix_multiplyWithScalar(m_matrix, scalar))
+            if (!error_isSuccess(matrix_multiplyWithScalar(m_matrix, scalar)))
                 throw new MatrixException(matrix_multiplyWithScalar(m_matrix, scalar));
         }
 };
