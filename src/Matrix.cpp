0151ebff0871d5030deba265e93a9f92583a7909
Yair Koskas <yairi2003@gmail.com>
2020-08-17 12:06:40 +0300

    added function declarations in Matrix class

---
+++ src/Matrix.cpp
@@ -0,0 +1,21 @@
+#include "Matrix.h"
+#include "ErrorCode.h"
+class Matrix{
+    public:
+        Matrix(PMatrix* m, uint32_t height, uint32_t width);
+        Matrix create(const Matrix& m);
+        
+        Matrix& operator=(const Matrix& m);
+        Matrix(Matrix&& m) noexcept;
+
+        Matrix& operator=(Matrix&& m) noexcept;
+        ~Matrix();
+
+        Matrix copy(PMatrix* result, CPMatrix source);
+        int getHeight(CPMatrix m);
+        int getWidth(CPMatrix m);
+        void setValue(PMatrix m, int rowIndex, int colIndex, double value);
+        Matrix add(CPMatrix lhs, CPMatrix rhs); 
+        Matrix multMatrix(CPMatrix lhs, CPMatrix rhs); 
+        Matrix multMatrixWithScalar(CPMatrix m, int scalar); 
+};
