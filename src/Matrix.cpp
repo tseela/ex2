d0b7e970182eaa859196816c47c04db554918072
tseela <tseelagamzo@gmail.com>
2020-08-17 12:16:56 +0300

    Changed declerations into functions. Not realized yet.

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -2,20 +2,102 @@
 #include "ErrorCode.h"
 class Matrix{
     public:
-        Matrix(PMatrix* m, uint32_t height, uint32_t width); //regular constuctor
-        Matrix(const Matrix& m); //copy constructor
+        Matrix(PMatrix* m, uint32_t height, uint32_t width) {
+
+
+
+
+        }
+        Matrix(const Matrix& m) {
+
+
+
+
+        }
         
-        Matrix& operator=(const Matrix& m); //copy assignment operator
-        Matrix(Matrix&& m) noexcept; //move constructor
+        Matrix& operator=(const Matrix& m) {
 
-        Matrix& operator=(Matrix&& m) noexcept; //move assignment constructor
-        ~Matrix(); //destructor
 
-        Matrix copy(PMatrix* result, CPMatrix source); //copy
-        int getHeight(CPMatrix m); //accessor
-        int getWidth(CPMatrix m); //accessor
-        void setValue(PMatrix m, int rowIndex, int colIndex, double value); //setter
-        Matrix add(CPMatrix lhs, CPMatrix rhs); 
-        Matrix multMatrix(CPMatrix lhs, CPMatrix rhs); 
-        Matrix multMatrixWithScalar(CPMatrix m, int scalar); 
+
+
+        }
+        Matrix(Matrix&& m) noexcept {
+
+
+
+
+        }
+
+        Matrix& operator=(Matrix&& m) noexcept {
+
+
+
+
+        }
+        ~Matrix() {
+
+
+
+        }
+
+        Matrix copy(PMatrix* result, CPMatrix source) {
+
+
+
+
+
+
+
+        }
+        int getHeight(CPMatrix m) {
+
+
+
+
+
+
+        }
+        int getWidth(CPMatrix m) {
+
+
+
+
+
+
+
+        }
+        void setValue(PMatrix m, int rowIndex, int colIndex, double value) {
+
+
+
+
+
+
+
+        }
+        Matrix add(CPMatrix lhs, CPMatrix rhs) {
+
+
+
+
+
+
+        }
+        Matrix multMatrix(CPMatrix lhs, CPMatrix rhs) {
+
+
+
+
+
+
+        }
+        Matrix multMatrixWithScalar(CPMatrix m, int scalar) {
+
+
+
+
+
+
+
+        }
 };
