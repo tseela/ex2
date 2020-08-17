e132596ff007decf506643b01b54c5f1569d6373
Yair Koskas <yairi2003@gmail.com>
2020-08-17 12:10:27 +0300

    added comments to declarations

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -2,19 +2,19 @@
 #include "ErrorCode.h"
 class Matrix{
     public:
-        Matrix(PMatrix* m, uint32_t height, uint32_t width);
-        Matrix create(const Matrix& m);
+        Matrix(PMatrix* m, uint32_t height, uint32_t width); //regular constuctor
+        Matrix(const Matrix& m); //copy constructor
         
-        Matrix& operator=(const Matrix& m);
-        Matrix(Matrix&& m) noexcept;
+        Matrix& operator=(const Matrix& m); //copy assignment operator
+        Matrix(Matrix&& m) noexcept; //move constructor
 
-        Matrix& operator=(Matrix&& m) noexcept;
-        ~Matrix();
+        Matrix& operator=(Matrix&& m) noexcept; //move assignment constructor
+        ~Matrix(); //destructor
 
-        Matrix copy(PMatrix* result, CPMatrix source);
-        int getHeight(CPMatrix m);
-        int getWidth(CPMatrix m);
-        void setValue(PMatrix m, int rowIndex, int colIndex, double value);
+        Matrix copy(PMatrix* result, CPMatrix source); //copy
+        int getHeight(CPMatrix m); //accessor
+        int getWidth(CPMatrix m); //accessor
+        void setValue(PMatrix m, int rowIndex, int colIndex, double value); //setter
         Matrix add(CPMatrix lhs, CPMatrix rhs); 
         Matrix multMatrix(CPMatrix lhs, CPMatrix rhs); 
         Matrix multMatrixWithScalar(CPMatrix m, int scalar); 
