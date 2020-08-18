a2a7872586424e9d013f6782119df11308275847
Yair Koskas <yairi2003@gmail.com>
2020-08-18 17:41:06 +0300

    added initial functions to bmp_tester.cpp

---
+++ src/bmp_tester.cpp
@@ -0,0 +1,9 @@
+#include "bmp_tester.hpp"
+void testing::bmp::rotate_image(const std::string& imagePath, const std::string& outputPath) {
+
+}
+
+
+void testing::bmp::convert_to_grayscale(const std::string& imagePath, const std::string& outputPath) {
+
+}
